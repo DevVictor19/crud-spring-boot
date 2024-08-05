51a35747a9923a4bddf491a47672b9d43e84f235
DevVictor19 <antoniovictor12@live.com>
2024-08-05 09:59:07 -0300

    feat: add required object is null ex

--- src/main/java/com/devvictor/user_crud_spring/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/devvictor/user_crud_spring/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -2,6 +2,7 @@ package com.devvictor.user_crud_spring.exceptions.handler;
 
 import com.devvictor.user_crud_spring.exceptions.ExceptionResponse;
 import com.devvictor.user_crud_spring.exceptions.NotFoundException;
+import com.devvictor.user_crud_spring.exceptions.RequiredObjectIsNullException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -42,5 +43,19 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         );
 
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RequiredObjectIsNullException.class)
+    public final ResponseEntity<ExceptionResponse> handleRequiredObjectIsNull(
+            RequiredObjectIsNullException ex,
+            WebRequest request) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
